c1279cb21ff73de7f6b18fa92d52a4d5d4b7e980
Zakaria j Young <zakskis8@gmail.com>
2021-06-06 19:40:31 +0000

    Implemented The current Ratio property method

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ import csv
 class Stock:
   def __init__(self,ticker):
     self.ticker = ticker
-    self.yfData = yf.Ticker(ticker).info
+    self.yfData = yf.Ticker(ticker)
   @property
   def sector(self):
-    return self.yfData['sector']
+    return self.yfData.info['sector']
   @property
   def payoutRatio(self):
     try:
-      return self.yfData['payoutRatio'] * 100
+      return self.yfData.info['payoutRatio'] * 100
     except:
       return 'Stock does not pay a dividend'
   @property
   def priceToBook(self):
-    return self.yfData['priceToBook']
+    return self.yfData.info['priceToBook']
   @property
   def marketCap(self):
-    return self.yfData['marketCap']
+    return self.yfData.info['marketCap']
   @property
   def currentDividendRate(self):
-    return self.yfData['dividendRate']
+    return self.yfData.info['dividendRate']
   @property
   def priorYearDividendRate(self):
-    return self.yfData['trailingAnnualDividendRate']
+    return self.yfData.info['trailingAnnualDividendRate']
   @property
   def DividendGrowthRate(self):
     try:
@@ -33,33 +33,42 @@ class Stock:
       return "Calculation not possible"
   @property
   def fiveYearAvgDividendRate(self):
-    return self.yfData['fiveYearAvgDividendRate']
+    return self.yfData.info['fiveYearAvgDividendRate']
   @property
   def fiftyTwoWeekHigh(self):
-    return self.yfData['fiftyTwoWeekHigh']
+    return self.yfData.info['fiftyTwoWeekHigh']
   @property
   def fiftyTwoWeekLow(self):
-    return self.yfData['fiftyTwoWeekLow']
+    return self.yfData.info['fiftyTwoWeekLow']
   @property
   def previousClose(self):
-    return self.yfData['previousClose']
+    return self.yfData.info['previousClose']
   @property
   def trailingPeRatio(self):
-    return self.yfData['trailingPE']
+    return self.yfData.info['trailingPE']
   @property
   def forwardPeRatio(self):
-    return self.yfData['forwardPE']
+    return self.yfData.info['forwardPE']
   @property
   def fourYearEarnings(self):
-    return dict(self.yfData['Earnings'])
+    try:
+      return dict(self.yfData.earnings['Earnings'])
+    except:
+      return None
   @property
   def fourYearRevenue(self):
-    return dict(self.yfData['Revenue'])
+    try:
+      return dict(self.yfData.earnings['Revenue'])
+    except:
+      return None
   @property
   def currentRatio(self):
-    #This is the annual current ratio
+    """
+    This is the MRQ current ratio and since the yfData dict doesnt have a key for current ratio 
+    we must Calculate it using Total current assets / Total current Liabilities
+    """
     stockData = self.yfData
-    needed_data = dict(stockData.balance_sheet[list(stockData.balance_sheet)[0]])
+    needed_data = dict(stockData.quarterly_balance_sheet[list(stockData.quarterly_balance_sheet)[0]])
     try:
       current_ratio = needed_data['Total Current Assets']/needed_data['Total Current Liabilities']
       return current_ratio
@@ -69,6 +78,7 @@ def run():
   with open('NYSE.csv','r') as nyse, open('NASDAQ.csv','r') as nasdaq:
     nyseReader = csv.DictReader(nyse)
     nasdaqReader = csv.DictReader(nasdaq)
-
+    msft = Stock('msft')
+    print(msft.fourYearRevenue)
 if __name__ == "__main__":
   run()
--- test.py
+++ test.py
@@ -5,8 +5,4 @@ with open('NYSE.csv','r') as nyse, open('NASDAQ.csv','r') as nasdaq:
   for stock in nyseReader:
     stock = stock['Ticker']
     stockData = yf.Ticker(f"{stock}")
-    needed_data = dict(stockData.balance_sheet[list(stockData.balance_sheet)[0]])
-    try:
-      current_ratio = needed_data['Total Current Assets']/needed_data['Total Current Liabilities']
-    except:
-      print('None')
+    
